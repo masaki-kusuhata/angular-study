be4e57ccedc6e7273824149f080c0c23f5e0b7f0
Masaki Kusuhata <masayan2oyugi@gmail.com>
2016-02-16 16:29:50 +0900

    多言語対応

--- app/directives/label/label.js
+++ app/directives/label/label.js
@@ -17,19 +17,28 @@
   ];
 
   /**
-   * LabelDirective
+   * 画面ID,項目IDを元に項目名称を取得するディレクティブ
    *
    * @class LabelDirective
    * @constructor
    */
   function LabelDirective(ItemDictionaryService) {
+
+    /**
+     * [画面ID,項目IDを元に項目名称を取得する]
+     * @param  {[object]} scope   [スコープ]
+     * @param  {[object]} element [エレメント]
+     * @param  {[object]} attrs   [属性]
+     */
     function labelLink(scope, element, attrs) {
-      console.log(attrs);
       ItemDictionaryService.get(
         attrs.lawsonFormid,
         attrs.lawsonLabelid
       ).then(function(res) {
         element[0].innerHTML = res;
+      })
+      .catch(function(ex) {
+        return ex;
       });
     }
 
